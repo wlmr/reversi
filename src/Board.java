080a51ecc944cf56942848fa12ccf67f256a6a62
andreas <mister.trattner@gmail.com>
2019-01-27 13:40:48 +0100

    in progress

--- src/Board.java
+++ src/Board.java
@@ -2,9 +2,9 @@ public class Board {
 
 
     private final static int BOARD__SIZE = 8;
-    private final static int WHITE = 1;
-    private final static int BLACK = -1;
-    private final static int EMPTY = 0;
+    public final static int WHITE = 1;
+    public final static int BLACK = -1;
+    public final static int EMPTY = 0;
 
     public static int getBoardSize()
     {
@@ -16,6 +16,7 @@ public class Board {
 
     private void initState()
     {
+        //TODO! add initial state which is not completely empty!
         for(int i = 0; i < BOARD__SIZE; i++)
         {
             for(int j = 0; j  < BOARD__SIZE; j++)
@@ -29,6 +30,28 @@ public class Board {
     {
         return state;
     }
+
+
+    public boolean isLegal(int row, int col)
+    {
+        for(int i = -1; i < 1; i++)
+        {
+            for(int j = -1; j < 1; j++) {
+                if (i * j == 0) continue;
+
+            }
+
+        }
+        return true;
+    }
+
+    public void setBrick(int row, int col, short color)
+    {
+        //Performs a few checks to see if the move is legal.
+
+    }
+
+
 
     public Board(){
         this.state = new short[BOARD__SIZE][BOARD__SIZE];
