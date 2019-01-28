dbcf98f051b24d0d161d82746b1ca21273a7f1c6
andreas <mister.trattner@gmail.com>
2019-01-28 10:17:17 +0100

    adds copy constructor

--- src/Board.java
+++ src/Board.java
@@ -7,28 +7,33 @@ public class Board {
     public final static int WHITE = 1;
     public final static int BLACK = -1;
     public final static int EMPTY = 0;
+    private int[][] state;
 
-    public static int getBoardSize()
-    {
+    public static int getBoardSize() {
         return BOARD_SIZE;
     }
 
-    private int [][] state;
 
+    public Board() {
+        this.state = new int[BOARD_SIZE][BOARD_SIZE];
+        initState();
+    }
 
-    private static int getOppositeColor(int color)
-    {
-        return (color == WHITE) ? BLACK: WHITE;
+    public Board(final Board other) {
+        this.state = new int[BOARD_SIZE][BOARD_SIZE];
+        initState(other.state);
     }
 
 
-    private void initState()
-    {
+    private static int getOppositeColor(int color) {
+        return (color == WHITE) ? BLACK : WHITE;
+    }
+
+
+    private void initState() {
         //TODO! add initial state which is not completely empty!
-        for(int i = 0; i < BOARD_SIZE; i++)
-        {
-            for(int j = 0; j  < BOARD_SIZE; j++)
-            {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
                 state[i][j] = EMPTY;
             }
         }
@@ -38,30 +43,35 @@ public class Board {
         state[4][4] = WHITE;
     }
 
-    public int [][] getState()
-    {
+
+    private void initState(int[][] otherState) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                this.state[i][j] = otherState[i][j];
+            }
+        }
+    }
+
+
+    public int[][] getState() {
         return state;
     }
 
 
-
-    boolean outsideBounds(int row, int col)
-    {
+    boolean outsideBounds(int row, int col) {
         return row < 0 || row >= BOARD_SIZE ||
                 col < 0 || col >= BOARD_SIZE;
     }
 
 
-    private boolean existsLine(int row, int col, int dx, int dy, int color)
-    {
+    private boolean existsLine(int row, int col, int dx, int dy, int color) {
         int opposite = getOppositeColor(color);
         int nbrOpposites = 0;
-        while(!outsideBounds(row, col))
-        {
+        while (!outsideBounds(row, col)) {
             int currentSlot = state[row][col];
-            if(currentSlot == EMPTY) return false;
-            else if(currentSlot == opposite) nbrOpposites++;
-            else if(currentSlot == color) return nbrOpposites > 0;
+            if (currentSlot == EMPTY) return false;
+            else if (currentSlot == opposite) nbrOpposites++;
+            else if (currentSlot == color) return nbrOpposites > 0;
             row = row + dy;
             col = col + dx;
         }
@@ -73,91 +83,82 @@ public class Board {
      * The move is considered legal if, in any direction (horizontal, vertical, diagonal),
      * there is a direct line of at least one brick of the opposite color, ended with a brick
      * of the same color. you can not place a brick on top of another brick, either.
+     *
      * @param row
      * @param col
      * @param color
      * @return
      */
-    public boolean isLegal(int row, int col, int color)
-    {
-        if(outsideBounds(row, col) || state[row][col] != EMPTY) return false;
-        for(int dx = -1; dx <= 1; dx++) //horizontal delta
+    public boolean isLegal(int row, int col, int color) {
+        if (outsideBounds(row, col) || state[row][col] != EMPTY) return false;
+        for (int dx = -1; dx <= 1; dx++) //horizontal delta
         {
-            for(int dy = -1; dy <= 1; dy++) { //Vertical delta
+            for (int dy = -1; dy <= 1; dy++) { //Vertical delta
                 boolean outside = outsideBounds(row + dy, col + dx);
                 if ((dx == 0 && dy == 0) || outside) continue;
-                if(existsLine(row + dy, col + dx, dx,dy, color)) return true;
+                if (existsLine(row + dy, col + dx, dx, dy, color)) return true;
             }
         }
         return false;
     }
 
-    public boolean setBrick(int row, int col, int color)
-    {
+    public boolean setBrick(int row, int col, int color) {
         //Performs a few checks to see if the move is legal.
-        if(outsideBounds(row, col)) return false;//|| !isLegal(row, col) || state[row][col] != EMPTY) return false;
+        if (outsideBounds(row, col)) return false;//|| !isLegal(row, col) || state[row][col] != EMPTY) return false;
         state[row][col] = color;
         update(row, col);
         return true;
     }
 
-    public ArrayList<int []> getLegalMoves(int color)
-    {
-        ArrayList<int []> legalMoves = new ArrayList<>();
-        for(int row = 0; row < BOARD_SIZE; row++)
-        {
-            for(int col = 0; col < BOARD_SIZE; col++){
-                if(isLegal(row, col, color)) legalMoves.add(new int []{row, col});
+    public ArrayList<int[]> getLegalMoves(int color) {
+        ArrayList<int[]> legalMoves = new ArrayList<>();
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                if (isLegal(row, col, color)) legalMoves.add(new int[]{row, col});
             }
         }
         return legalMoves;
     }
 
-    public int getNbrLegalMoves(int color)
-    {
+    public int getNbrLegalMoves(int color) {
         return getLegalMoves(color).size();
     }
 
-    private void invert(int row, int col)
-    {
+    private void invert(int row, int col) {
         state[row][col] = getOppositeColor(state[row][col]);
     }
 
     private void performFlips(int row, int col, int dx, int dy, int color) {
         ArrayList<int[]> foundFlips = new ArrayList<>();
         final int oppositeColor = getOppositeColor(color);
-        while(!outsideBounds(row, col))
-        {
+        while (!outsideBounds(row, col)) {
             int currentBrick = state[row][col];
-            if(currentBrick == EMPTY){
+            if (currentBrick == EMPTY) {
                 return;
-            }
-            else if(currentBrick == oppositeColor) {
+            } else if (currentBrick == oppositeColor) {
                 foundFlips.add(new int[]{row, col});
                 row = row + dy;
                 col = col + dx;
-            }
-            else{
+            } else {
                 break;
             }
         }
-        if(outsideBounds(row, col)) return;
-        for(int [] pair: foundFlips){
+        if (outsideBounds(row, col)) return;
+        for (int[] pair : foundFlips) {
             invert(pair[0], pair[1]);
         }
     }
 
 
-    private void applyRules(int row, int col)
-    {
+    private void applyRules(int row, int col) {
         int currentColor = state[row][col];
-        if(currentColor == EMPTY) return;
+        if (currentColor == EMPTY) return;
 
-        for(int dx = -1; dx <= 1; dx++){
-            for(int dy = -1; dy <= 1; dy++)
-            {
-                if(outsideBounds(row + dy, col + dx)) continue;
-                else if(dx == 0 && dy == 0) continue;;
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                if (outsideBounds(row + dy, col + dx)) continue;
+                else if (dx == 0 && dy == 0) continue;
+                ;
                 performFlips(row + dy, col + dx, dx, dy, currentColor);
 
             }
@@ -165,12 +166,10 @@ public class Board {
     }
 
 
-    public int getPointSum(){
+    public int getPointSum() {
         int count = 0;
-        for(int i = 0; i < BOARD_SIZE; i++)
-        {
-            for(int j = 0; j < BOARD_SIZE; j++)
-            {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
                 count += state[i][j];
             }
         }
@@ -178,25 +177,16 @@ public class Board {
     }
 
 
-    private void update(int row, int col)
-    {
+    private void update(int row, int col) {
         applyRules(row, col);
-    }
-
-
-    public Board(){
-        this.state = new int[BOARD_SIZE][BOARD_SIZE];
-        initState();
     }
 
 
     public int count(int color) {
         int count = 0;
-        for(int i = 0; i < BOARD_SIZE; i++)
-        {
-            for(int j = 0; j < BOARD_SIZE; j++)
-            {
-                if(state[i][j] == color) count++;
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                if (state[i][j] == color) count++;
             }
         }
         return count;
